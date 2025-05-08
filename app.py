7d16aec009ccefc6eb1bb35a0a4d631e7ac69857
Andrew-O39 <andyowan.aoa@gmail.com>
2025-05-08 23:51:02 +0200

    manually created posts.json data and also styled the page with style.css file in the static folder

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from json_data import load_posts
 
 app = Flask(__name__)
 
--- json_data.py
+++ json_data.py
@@ -10,3 +10,23 @@ def load_posts() -> List[Dict]:
 def save_posts(posts: List[Dict]):
     with open(POSTS_FILE, 'w') as file:
         json.dump(posts, file, indent=4)
+
+
+def add_post(author: str, title: str, content: str):
+    posts = load_posts()
+    new_id = max([post['id'] for post in posts], default=0) + 1
+    posts.append({'id': new_id, 'author': author, 'title': title, 'content': content})
+    save_posts(posts)
+
+def delete_post(post_id: int):
+    posts = load_posts()
+    posts = [post for post in posts if post['id'] != post_id]
+    save_posts(posts)
+
+def update_post(post_id: int, author: str, title: str, content: str):
+    posts = load_posts()
+    for post in posts:
+        if post['id'] == post_id:
+            post.update({'author': author, 'title': title, 'content': content})
+            break
+    save_posts(posts)
