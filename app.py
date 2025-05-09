e5cd7d4addabf758ff22689f856000adab0e4412
Andrew-O39 <andyowan.aoa@gmail.com>
2025-05-09 15:20:32 +0200

    create a /add route in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from json_data import load_posts
+from flask import Flask, render_template, request, redirect, url_for
+from json_data import load_posts, add_post
 
 app = Flask(__name__)
 
@@ -8,6 +8,16 @@ app = Flask(__name__)
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+        add_post(author, title, content)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
